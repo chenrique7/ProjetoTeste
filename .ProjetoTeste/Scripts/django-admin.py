41f2c83c78e2be0bd49c47d0370db694e59fd17b
CARLOS HENRIQUE SOUZA DOS SANTOS <carlos.santos8@estudante.ifto.edu.br>
2019-08-09 10:33:42 -0300

    primeiro commit

---
+++ .ProjetoTeste/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\03627073240\documents\projetos\projetoteste\.projetoteste\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
